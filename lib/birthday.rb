00ca3177f64620e2a3ae9cef4fc3eb304de40b4b
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-25 00:13:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,9 +11,9 @@ def happy_birthday(birthday_kids)
   end
 end
 
-#def age_appropriate_birthday(age_appropriate)
- # age_appropriate.each do |name, age_appropriate|
-  #  puts "Happy Birthday #{name}! You are now #{age_appropriate} years old!"
-  #end
-#end
+def age_appropriate_birthday(age_appropriate)
+  age_appropriate.each do |name, age_appropriate|
+    puts "Happy Birthday #{name}! You are now {age_appropriate} years old!"
+  end
+end
 
